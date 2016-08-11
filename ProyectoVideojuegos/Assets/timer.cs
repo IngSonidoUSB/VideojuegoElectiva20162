bf6b47b1842ba28e402c4b7a354e966586cf0102
Ingenieria de Sonido <miguelolivares@MacBook-Air-de-Miguel.local>
2016-08-11 08:00:43 -0500

    Inicio Proyecto Videojuegos
    Inicio Proyecto Videojuegos

---
+++ ProyectoVideojuegos/Assets/timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+	 
+	public Text timertext;
+	private float tiempo;
+	public int tempo = 120;
+	public int compas = 4;
+	// Use this for initialization
+	void Start () {
+		tiempo = Time.deltaTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tiempo += Time.deltaTime;
+		float multiplicador = tempo / 60;
+		int contador = (int) (tiempo * multiplicador) + 1;
+		timertext.text = contador.ToString ();
+		if (contador == (compas+1)) {
+			
+			tiempo = 0;
+		}
+
+	
+	}
+}
